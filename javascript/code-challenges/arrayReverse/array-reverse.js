2e00ff7f5d5617f42b382b77589ba25192931660
ZenAtomsk <ricardo.b3788@gmail.com>
2020-12-07 21:34:16 -0800

    fixed some syntax errors from being written on my whiteboard

--- javascript/code-challenges/arrayReverse/array-reverse.js
+++ javascript/code-challenges/arrayReverse/array-reverse.js
@@ -3,10 +3,10 @@
 let arr = [1, 2, 3, 4, 5, 6, 7]
 
 function reverseArray(arr){
-  for (var i = 0; i <= (arr.length - 1) / 2); i++){
-      let tempArr = arr[ i ];
-      arr[ i ] = arr[arr.length - 1 - [ i ];
-      arr[arr.length - 1 - [ i ]] = tempArr;
+  for (var i = 0; i <= (arr.length - 1) /2; i++){
+      let tempArr = arr[i];
+      arr[i] = arr[arr.length - 1 -  i];
+      arr[arr.length - 1 -  i ] = tempArr;
    }
    return arr;
 }
