79664169ba41b8f74ad9fe78b7fb9ef685f1c993
ZenAtomsk <ricardo.b3788@gmail.com>
2020-12-27 21:58:23 -0800

    cc15 set up

---
+++ javascript/code-challenges/tree/tree.js
@@ -0,0 +1,41 @@
+'use strict'
+
+class Node{
+  constructor(value=null, leftChild=null, rightChild=null){
+    this.value = value;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+  }
+}
+
+class BinaryTree{
+  constructor(){
+
+  }
+
+  preOrder(){
+
+  }
+
+  inOrder(){
+
+  }
+
+  postOrder(){
+
+  }
+}
+
+class BinarySearchTree{
+  constructor(){
+
+  }
+
+  add(){
+
+  }
+
+  contains(){
+    
+  }
+}
